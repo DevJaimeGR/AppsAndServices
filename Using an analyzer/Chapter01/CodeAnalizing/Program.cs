a5013cd3548366c17fcb17f0cf7b4ba13c18bf1b
DevJaimeGR <140999790+DevJaimeGR@users.noreply.github.com>
2023-08-21 12:55:07 -0500

    Add files via upload

---
+++ Using an analyzer/Chapter01/CodeAnalizing/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="Logic">
+// Copyright (c) Logic. All rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+
+namespace CodeAnalyzing;
+
+/// <summary>
+/// the main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// the main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger");
+    }
+}
