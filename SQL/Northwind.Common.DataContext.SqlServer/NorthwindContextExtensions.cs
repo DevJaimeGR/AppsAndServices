1293e31d59b6975e6256fd5cc18a74f2d8acf169
DevJaimeGR <140999790+DevJaimeGR@users.noreply.github.com>
2023-08-21 13:06:37 -0500

    Add files via upload

---
+++ SQL/Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Packt.Shared;
+
+public static class NorthwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the SqlServer database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="connectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    public static IServiceCollection AddNorthwindContext(
+        this IServiceCollection services,
+        string connectionString = "Data Source=DDC-CO-JGONZALE;Initial Catalog=Northwind;Persist Security Info=True;User ID=sa;Password=admin2023;TrustServerCertificate=True;")
+    {
+        services.AddDbContext<NorthwindContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.LogTo(Console.WriteLine,
+        new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+        });
+        return services;
+    }
+}
---
+++ SQL/Northwind.Common.DataContext.SqlServer/SetLastRefreshedInterceptor.cs
@@ -0,0 +1,18 @@
+﻿// IMaterializationInterceptor, MaterializationInterceptionData
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Northwind.Common.EntityModels.SqlServer;
+
+namespace Packt.Shared;
+public class SetLastRefreshedInterceptor : IMaterializationInterceptor
+{
+    public object InitializedInstance(
+    MaterializationInterceptionData materializationData,
+    object entity)
+    {
+        if (entity is IHasLastRefreshed entityWithLastRefreshed)
+        {
+            entityWithLastRefreshed.LastRefreshed = DateTimeOffset.UtcNow;
+        }
+        return entity;
+    }
+}
